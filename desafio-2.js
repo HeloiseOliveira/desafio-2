da53fa50b82cfcb3d12bc7d5c16c6472da4bf369
Heloise Oliveira <113391588+HeloiseOliveira@users.noreply.github.com>
2023-10-29 13:05:27 -0300

    Add files via upload

---
+++ desafio-2.js
@@ -0,0 +1,29 @@
+function ranking(){
+	let vitorias = 70;
+	let derrotas = 5;
+	let res = vitorias - derrotas;
+	return res
+    }
+    
+    let saldo = ranking()
+    let nivel;
+
+    
+    if (saldo < 10){
+    nivel = "Ferro";
+    } else if (saldo >= 11 && saldo <= 20){
+    nivel = "Bronze";
+    } else if ( saldo >= 21 && saldo <= 50){
+    nivel = "Prata";
+    } else if ( saldo >= 51 && saldo <= 80){
+    nivel = "Ouro";
+    } else if ( saldo >= 81 && saldo <= 90){
+    nivel = "Diamante";
+    } else if ( saldo >= 91 && saldo <= 100){
+    nivel = "Lendário";
+    } else {
+    nivel = "Imortal";
+    }
+    
+   
+    console.log(`O Herói tem saldo ${saldo} está no nível de ${nivel}`);
